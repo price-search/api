f6eb6bab18853e755653c3f4d96b725e923e8f24
Felipe Santos <felipecassiors@gmail.com>
2020-05-15 03:10:17 +0000

    ci(reviewdog): try to catch an error

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { Product } from './products/product.entity'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
