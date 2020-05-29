a6f8b0022d8be900ba176fa2de1abec44050c54c
Felipe Santos <felipecassiors@gmail.com>
2020-05-29 00:31:18 +0000

    chore: adjust environment variables

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
 module.exports = {
   type: 'postgres',
-  host: 'db',
-  port: 5432,
-  username: 'user',
-  password: 'pass',
-  database: 'data',
+  host: process.env.TYPEORM_HOST || 'db',
+  port: parseInt(process.env.TYPEORM_PORT, 10) || 5432,
+  username: process.env.TYPEORM_USERNAME || 'user',
+  password: process.env.TYPEORM_PASSWORD || 'pass',
+  database: process.env.TYPEORM_DATABASE || 'data',
   entities: [
     process.env.NODE_ENV === 'test'
       ? 'src/**/*.entity.ts'
