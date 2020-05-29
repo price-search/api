c0aa791bdd366514f1bdc7ba5b61183e825e0c45
Felipe Santos <felipecassiors@gmail.com>
2020-05-29 02:05:06 +0000

    feat: add users and shopping lists

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,18 @@ import { ProductsModule } from './products/products.module';
 import { Connection } from 'typeorm';
 import { ShopsModule } from './shops/shops.module';
 import { OffersModule } from './offers/offers.module';
+import { ShoppingListsModule } from './shopping-lists/shopping-lists.module';
+import { UserModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), ProductsModule, ShopsModule, OffersModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    ProductsModule,
+    ShopsModule,
+    OffersModule,
+    UserModule,
+    ShoppingListsModule,
+  ],
   controllers: [],
   providers: [],
 })
