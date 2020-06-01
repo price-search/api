859fe51e5f14b3027d05c6d4db837df922662d7b
Felipe Santos <felipecassiors@gmail.com>
2020-06-01 23:41:09 +0000

    chore: fix database in heroku

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,11 @@
 module.exports = {
   type: 'postgres',
-  host: process.env.TYPEORM_HOST || 'db',
-  port: parseInt(process.env.TYPEORM_PORT, 10) || 5432,
-  username: process.env.TYPEORM_USERNAME || 'user',
-  password: process.env.TYPEORM_PASSWORD || 'pass',
-  database: process.env.TYPEORM_DATABASE || 'data',
+  url:
+    process.env.DATABASE_URL ||
+    `postgres://${process.env.TYPEORM_USERNAME || 'user'}:${process.env
+      .TYPEORM_PASSWORD || 'pass'}@${process.env.TYPEORM_HOST ||
+      'db'}:${parseInt(process.env.TYPEORM_PORT, 10) || 5432}/${process.env
+      .TYPEORM_DATABASE || 'data'}`,
   entities: [
     process.env.NODE_ENV === 'test'
       ? 'src/**/*.entity.ts'
