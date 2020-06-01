94c763c52e9d77b9c7621be4cd76f051da1a26e4
Felipe Santos <felipecassiors@gmail.com>
2020-06-01 21:27:55 +0000

    chore: fix swagger on azure

--- src/main.azure.ts
+++ src/main.azure.ts
@@ -1,11 +1,16 @@
 import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export async function createApp(): Promise<INestApplication> {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  
+
+  const options = new DocumentBuilder().build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.init();
   return app;
 }
