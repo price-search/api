17161a7da00afc790e48933bd07e0dbe14978a1a
Felipe Santos <felipecassiors@gmail.com>
2020-06-02 00:53:46 -0300

    chore(devcontainer): add nest, commitizen and heroku (#19)

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,7 @@
 const type = process.env.TYPEORM_CONNECTION || 'postgres';
 const username = process.env.TYPEORM_USERNAME || 'user';
 const password = process.env.TYPEORM_PASSWORD || 'pass';
-const host = process.env.TYPEORM_HOST || 'db';
+const host = process.env.TYPEORM_HOST || 'postgres';
 const port = parseInt(process.env.TYPEORM_PORT, 10) || 5432;
 const database = process.env.TYPEORM_DATABASE || 'data';
 
