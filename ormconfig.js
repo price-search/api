d6ac49c1d3b29d1865d1eb16c97eaa61fb64c706
Felipe Santos <felipecassiors@gmail.com>
2020-06-01 21:36:37 -0300

    chore: deploy to heroku (#13)

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,15 @@
+const type = process.env.TYPEORM_CONNECTION || 'postgres';
+const username = process.env.TYPEORM_USERNAME || 'user';
+const password = process.env.TYPEORM_PASSWORD || 'pass';
+const host = process.env.TYPEORM_HOST || 'db';
+const port = parseInt(process.env.TYPEORM_PORT, 10) || 5432;
+const database = process.env.TYPEORM_DATABASE || 'data';
+
 module.exports = {
-  type: 'postgres',
-  host: process.env.TYPEORM_HOST || 'db',
-  port: parseInt(process.env.TYPEORM_PORT, 10) || 5432,
-  username: process.env.TYPEORM_USERNAME || 'user',
-  password: process.env.TYPEORM_PASSWORD || 'pass',
-  database: process.env.TYPEORM_DATABASE || 'data',
+  type,
+  url:
+    process.env.DATABASE_URL ||
+    `${type}://${username}:${password}@${host}:${port}/${database}`,
   entities: [
     process.env.NODE_ENV === 'test'
       ? 'src/**/*.entity.ts'
