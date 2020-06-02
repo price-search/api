c8ceaa9c97813d8adbd4d51e015cd245358d7dd2
Felipe Santos <felipecassiors@gmail.com>
2020-06-02 00:29:03 +0000

    chore: set final url

--- ormconfig.js
+++ ormconfig.js
@@ -1,3 +1,4 @@
+const type = process.env.TYPEORM_CONNECTION || 'postgres';
 const username = process.env.TYPEORM_USERNAME || 'user';
 const password = process.env.TYPEORM_PASSWORD || 'pass';
 const host = process.env.TYPEORM_HOST || 'db';
@@ -5,10 +6,10 @@ const port = parseInt(process.env.TYPEORM_PORT, 10) || 5432;
 const database = process.env.TYPEORM_DATABASE || 'data';
 
 module.exports = {
-  type: 'postgres',
+  type,
   url:
     process.env.DATABASE_URL ||
-    `postgres://${username}:${password}@${host}:${port}/${database}`,
+    `${type}://${username}:${password}@${host}:${port}/${database}`,
   entities: [
     process.env.NODE_ENV === 'test'
       ? 'src/**/*.entity.ts'
--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,5 @@ async function bootstrap() {
   SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(process.env.PORT || 3000);
-
-  console.log(`DATABASE_URL: ${process.env.DATABASE_URL}`);
 }
 bootstrap();
