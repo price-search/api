4eb24108b263b07e8780d42e25b39d3625086935
Felipe Santos <felipecassiors@gmail.com>
2020-06-04 03:02:25 +0000

    chore(heroku) optimize heroku

--- ormconfig.js
+++ ormconfig.js
@@ -4,16 +4,18 @@ const password = process.env.TYPEORM_PASSWORD || 'pass';
 const host = process.env.TYPEORM_HOST || 'postgres';
 const port = parseInt(process.env.TYPEORM_PORT, 10) || 5432;
 const database = process.env.TYPEORM_DATABASE || 'data';
+const entities = [
+  process[Symbol.for('ts-node.register.instance')] || // In case we are running from ts-node
+  process.env.NODE_ENV === 'test' // In case we are running from jest
+    ? 'src/**/*.entity.ts'
+    : 'dist/**/*.entity.js',
+];
 
 module.exports = {
   type,
   url:
     process.env.DATABASE_URL ||
     `${type}://${username}:${password}@${host}:${port}/${database}`,
-  entities: [
-    process.env.NODE_ENV === 'test'
-      ? 'src/**/*.entity.ts'
-      : 'dist/**/*.entity.js',
-  ],
+  entities,
   synchronize: true,
 };
