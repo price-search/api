50953392a7356eb9076dbdf98d23e4bb9135038b
Thalis Andrade Oliveira de Souza <thalisaosouza@hotmail.com>
2020-06-11 19:11:13 +0000

    Remove await

--- src/database/seeds/3-create-offers.seed.ts
+++ src/database/seeds/3-create-offers.seed.ts
@@ -4,8 +4,7 @@ import { Connection } from 'typeorm';
 import { Offer } from '../../offers/offer.entity';
 
 export default class CreateOffers implements Seeder {
-  public async run(factory: Factory, connection: Connection): Promise<any> { 
-    await factory(Offer)().createMany(100);
+  public async run(factory: Factory, connection: Connection): Promise<any> {
     await connection
       .createQueryBuilder()
       .insert()
